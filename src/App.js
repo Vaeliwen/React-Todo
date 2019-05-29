85ca49250cd49e1d2dc001586e784589b527d813
Vaeliwen <49922103+Vaeliwen@users.noreply.github.com>
2019-05-29 17:53:42 -0400

    Added component for form/buttons.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React from "react";
+
+import FormComponent from "./components/TodoComponents/TodoForm.js";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      task: "this",
+      id: "Time Stamp",
+      completed: "false"
+    };
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <FormComponent />
       </div>
     );
   }
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FormComponent = () => {
+  return (
+    <div class="form">
+      <input placeholder="What needs doing?" />
+      <button>Add Todo</button>
+      <button>Clear Completed</button>
+    </div>
+  );
+};
+
+export default FormComponent;
